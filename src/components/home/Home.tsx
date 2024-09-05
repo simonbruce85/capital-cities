9b161f562e67f426aa6cfe9d946be63784b8204e
Simon <simonbrucepuma@gmail.com>
2024-09-05 01:01:41 -0400

    styling

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './Home.css';
-import backgroundImage from '../../assets/background.jpg';  // Import the background image
-import homeImage from '../../assets/home.png'
+import homeImage from '../../assets/logo.png'
 import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
     return (
-        <div className="container" style={{ backgroundImage: `url(${backgroundImage})` }}>
+        <div className="container">
             <div className="overlay"></div> {/* Black overlay with opacity */}
             <div className="content">
-                <img src={homeImage} style={{ width: '250px', height: "250px", paddingBottom: "100px" }}></img>
+                <img src={homeImage} style={{ width: '250px', height: "250px", paddingBottom: "20px" }}></img>
+                <h2 style={{paddingBottom: "100px", color:"white", fontSize:"36px"}}>GeoQuiz!</h2>
                 <Link to="/byCountry">
                     <div className="start-button">Start</div>
                 </Link>
